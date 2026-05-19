3ddd9c312671cee972253f277eafe64c25463cf3
johnhicktonhughes-sketch <johnhicktonhughes@gmail.com>
2026-05-19 18:36:42 +0100

    Add Alembic and initial migration

---
+++ alembic/env.py
@@ -0,0 +1,58 @@
+from logging.config import fileConfig
+import os
+from sqlalchemy import create_engine
+from sqlalchemy import pool
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# Import the Flask app and SQLAlchemy metadata
+from app import create_app
+from database import db
+
+app = create_app()
+
+with app.app_context():
+    target_metadata = db.metadata
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = app.config["SQLALCHEMY_DATABASE_URI"]
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    connectable = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], poolclass=pool.NullPool)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            render_as_batch=True,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
